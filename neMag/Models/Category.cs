2b100ff4140935bc3105fb588c4b1befe545411f
FLORIN VASILIU <florin.vasiliu@s.unibuc.ro>
2021-03-15 08:58:18 +0200

    Baza de date + migratii.

---
+++ neMag/Models/Category.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace neMag.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Required(ErrorMessage = "Title is mandatory!")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Description is mandatory!")]
+        public string Description { get; set; }
+
+        // public virtual ICollection<Topic> Topics { get; set; }
+    }
+}
