dfd17edc1a3a86e83afeee3c1be80805bcedbb05
JustMike13 <62668963+JustMike13@users.noreply.github.com>
2021-04-07 14:47:22 +0300

    add Order with status="Cos"and new user's id when creating account

--- neMag/Controllers/AccountController.cs
+++ neMag/Controllers/AccountController.cs
@@ -160,7 +160,9 @@ namespace neMag.Controllers
                 {
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
 
-                    UserManager.AddToRole(user.Id, "User"); 
+                    UserManager.AddToRole(user.Id, "User");
+                    var order = new Order { UserId = user.Id, Date = DateTime.Now, Status = "Cos" };
+                    db.Orders.Add(order);
                     db.SaveChanges();
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
